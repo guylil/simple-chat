276509566668b354ad51080a9efb726531c6c35e
G <guylilian@gmail.com>
2019-11-10 22:18:56 +0200

    users list added

--- server.js
+++ server.js
@@ -16,14 +16,19 @@ app.get('/', function(req, res){
     res.sendFile(__dirname +'/index.html');
 });
 
+let userNames = [];
 io.on('connection', function(socket){
     console.log('a user connected: ', socket.id);
+    userNames.push(socket.id);
+    io.emit('users list', userNames);
     socket.on('disconnect', function(){
         console.log('user disconnected ',socket.id);
+        userNames = userNames.filter(id => id !== socket.id);
+        io.emit('users list', userNames);
     });
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
-        console.log('message: ' + msg);
+        console.log('id: ',socket.id,' message: ' + msg);
     });
 });
 server.listen(3000, function(){
